880e445354b67e232e9f9f25e5bc3e04987f663e
SangarajuDinesh <sangarajudinesh@gmail.com>
2023-11-14 12:17:38 +0530

    Thread creation using Runnable Interface

---
+++ RunnableInterface/PrintNumbers.java
@@ -0,0 +1,20 @@
+package com.kn.RunnableInterface;
+
+public class PrintNumbers implements Runnable
+{
+
+	@Override
+	public void run()
+	{
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("Number = "+i);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
